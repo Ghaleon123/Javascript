eaa75eca58d4f95bac15795ffb42c27d94ebfad1
Galeo <Arichikari@yandex.ru>
2023-02-14 14:24:46 +0300

    4

---
+++ howework2/3/hw3.js
@@ -0,0 +1,8 @@
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+  
+console.log("Random Number between 1 and 5: ")
+  
+// Function call
+console.log( randomNumber(1, 5) );
